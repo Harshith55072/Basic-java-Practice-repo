9b63a5faaabb4ec793970e50fbcfe3f173839aaf
Harshith55072 <bharshith157@gmail.com>
2026-07-28 10:20:11 +0530

    Device class

---
+++ src/strings/Device.java
@@ -0,0 +1,36 @@
+
+
+class Device
+{
+    void deviceType()
+    {
+        System.out.println("I am an electronic device");
+    }
+}
+class Phone extends Device
+{
+    void brand()
+    {
+        System.out.println("Brand : samsung");
+    }
+}
+class SmartPhone extends Phone
+{
+    void feature()
+    {
+        System.out.println("Features, Touchscreen,Camera, Internet");
+    }
+}
+
+
+ class MultiLevelInheritanceDemo {
+
+    public static void main(String[] args) {
+        SmartPhone smart=new SmartPhone();
+        smart.deviceType();
+        smart.brand();
+        smart.feature();
+
+    }
+
+}
