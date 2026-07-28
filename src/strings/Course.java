ae8ed8c6b26a13ea2c155a619ec80c3c627d7785
Harshith55072 <bharshith157@gmail.com>
2026-07-28 10:17:28 +0530

    Cource class

---
+++ src/strings/Course.java
@@ -0,0 +1,33 @@
+
+
+//parent class
+class Course
+{
+    String courseName="Java Programmming"; //instance var
+
+    void showCourse()
+    {
+        System.out.println("Course :"+courseName);
+    }
+}
+//sub class
+class Student extends Course
+{
+    String name="Ali";
+
+    void showStudent()
+    {
+        System.out.println("Student : "+name);
+    }
+}
+ class SingleLevelInheritanceDemo {
+
+    public static void main(String[] args) {
+        Student student=new Student();
+        student.showCourse();
+        student.showStudent();
+
+
+    }
+
+}
