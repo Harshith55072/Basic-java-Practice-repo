6390d94b436cdb79f8a2a7c5e07e6366c7e8b1cb
Harshith55072 <bharshith157@gmail.com>
2026-07-29 10:11:50 +0530

    notification

---
+++ src/strings/SuperWithVariable.java
@@ -0,0 +1,9 @@
+public class SuperWithVariable {
+
+    public static void main(String[] args) {
+        PushNotification obj=new  PushNotification();
+        obj.showNotification();
+
+    }
+
+}
