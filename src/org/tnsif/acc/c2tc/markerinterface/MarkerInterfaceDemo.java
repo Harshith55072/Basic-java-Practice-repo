7da5d4c74857428d87ed9d28133b9a9c94b3ce9f
Harshith55072 <bharshith157@gmail.com>
2026-08-11 10:45:37 +0530

    Fixed compile issue with intelij.

---
+++ src/org/tnsif/acc/c2tc/markerinterface/MarkerInterfaceDemo.java
@@ -0,0 +1,21 @@
+package org.tnsif.acc.c2tc.markerinterface;
+
+public class MarkerInterfaceDemo {
+    public static void main(String[] args) {
+        Registerable s = new Student(12, "Sushma", 20000, "Java Programming");
+        Registerable s1 = new Student(13, "Anil", 30000, "Java Programming");
+
+        
+        if (s instanceof Registerable) {
+            System.out.println("Student is registered for this course");
+        } else {
+            System.out.println("Student is not registered for this course");
+        }
+
+        if (s1 instanceof Registerable) {
+            System.out.println("Student is registered for this course");
+        } else {
+            System.out.println("Student is not registered for this course");
+        }
+    }
+}
